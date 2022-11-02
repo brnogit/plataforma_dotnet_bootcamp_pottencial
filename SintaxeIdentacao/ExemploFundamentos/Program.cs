efbd2c81535d5ebeabb14aa0ace4f20d4f0dd910
bmotadev <bmotadev@gmail.com>
2022-11-02 14:30:09 -0300

    manipulando variaveis

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -1,18 +1,25 @@
 ﻿using ExemploFundamentos.Models;
 
 string apresentacao = "Olá, seja bem vindo";
+
 int quantidade = 1;
-double altura = 1.80;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+
+
+/* double altura = 1.80;
 
 decimal preco = 1.80m;
+bool condicao = true; */
 
-bool condicao = true;
-
-Console.WriteLine(apresentacao);
+/* Console.WriteLine(apresentacao);
 Console.WriteLine("Valor da varíavel quantidade: " + quantidade);
 Console.WriteLine("Valor da varíavel altura: " + altura.ToString("0.00"));
 Console.WriteLine("Valor da varíavel preco: " + preco);
-Console.WriteLine("Valor da varíavel condicao: " + condicao);
+Console.WriteLine("Valor da varíavel condicao: " + condicao); */
 
 
 //Pessoa pessoa1 = new Pessoa();
