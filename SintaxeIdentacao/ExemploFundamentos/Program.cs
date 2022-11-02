f21387cd27fa8e9b48eca5acd9acb8122d113fcb
bmotadev <bmotadev@gmail.com>
2022-11-02 16:59:55 -0300

    ordem dos operadores

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -1,5 +1,11 @@
 ﻿using ExemploFundamentos.Models;
 
+double a = 4 / (2 + 2);
+
+Console.WriteLine(a);
+
+
+
 // int a = 5;
 // double b = a;
 
@@ -9,10 +15,10 @@
 //long a = long.MaxValue;
 //int b = Convert.ToInt32(a);
 
-int a = int.MaxValue;
-long b = a;
+// int a = int.MaxValue;
+// long b = a;
 
-Console.WriteLine(b);
+// Console.WriteLine(b);
 
 
 
