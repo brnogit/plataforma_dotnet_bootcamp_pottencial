6aad97652f3180c909715055a18c360b685b2e90
bmotadev <bmotadev@gmail.com>
2022-11-03 15:57:06 -0300

    construindo um menu interativo

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -1,16 +1,78 @@
 ﻿using ExemploFundamentos.Models;
 
+string opcao;
 
-int soma = 0, numero = 0;
-
-do
+while(true)
 {
-    Console.WriteLine("Digite um número (0 para parar)");
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
 
-    soma += numero;
-} while(numero != 0);
+    opcao = Console.ReadLine();
 
-Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+    switch(opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero = 0;
+
+// do
+// {
+//     Console.WriteLine("Digite um número (0 para parar)");
+
+//     soma += numero;
+// } while(numero != 0);
+
+// Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
 
 
