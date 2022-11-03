aef873747821adb02932410d046149fd394a39d7
bmotadev <bmotadev@gmail.com>
2022-11-03 15:30:26 -0300

    debugando o DO WHILE

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -6,7 +6,6 @@ int soma = 0, numero = 0;
 do
 {
     Console.WriteLine("Digite um número (0 para parar)");
-    numero = Convert.ToInt32(Console.ReadLine());
 
     soma += numero;
 } while(numero != 0);
