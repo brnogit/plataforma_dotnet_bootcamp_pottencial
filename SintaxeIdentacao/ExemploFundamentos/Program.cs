eba690caf47b719625b67a3c54b532be8526931a
bmotadev <bmotadev@gmail.com>
2022-11-03 15:20:09 -0300

    interrompendo o fluxo de execucao

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -9,6 +9,12 @@ while (contador <= 10)
     Console.WriteLine($"{contador + 1} Execução: {numero} x {contador} = {numero * contador}");
     contador++;
 
+    if(contador == 6)
+    {
+        break;
+    }
+    
+
 }
 
 
