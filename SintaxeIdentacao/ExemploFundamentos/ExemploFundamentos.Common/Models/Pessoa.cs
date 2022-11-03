41b25c8abbc1253369bc51b57af05403eea284a9
bmotadev <bmotadev@gmail.com>
2022-11-03 19:39:00 -0300

    organizando e referenciando projetos

--- SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
+++ SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ExemploFundamentos.Models
+namespace ExemploFundamentos.Common.Models
 {
     public class Pessoa
     {
--- SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -1,44 +1,74 @@
-﻿using ExemploFundamentos.Models;
+﻿using ExemploFundamentos.Common.Models;
 
-string opcao;
-bool exibirMenu = true;
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Bruno";
+pessoa1.Idade = 20;
+pessoa1.Apresentar();
 
-while(exibirMenu)
-{
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar cliente");
-    Console.WriteLine("2 - Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
-    Console.WriteLine("4 - Encerrar");
 
-    opcao = Console.ReadLine();
 
-    switch(opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastro de cliente");
-            break;
 
-        case "2":
-            Console.WriteLine("Busca de cliente");
-            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string opcao;
+// bool exibirMenu = true;
+
+// while(exibirMenu)
+// {
+//     Console.Clear();
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Busca de cliente");
+//             break;
         
-        case "3":
-            Console.WriteLine("Apagar cliente");
-            break;
+//         case "3":
+//             Console.WriteLine("Apagar cliente");
+//             break;
 
-        case "4":
-            Console.WriteLine("Encerrar");
-            exibirMenu = false;
-            break;
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             exibirMenu = false;
+//             break;
 
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
 
-    }
-}
+//     }
+// }
 
 
 
