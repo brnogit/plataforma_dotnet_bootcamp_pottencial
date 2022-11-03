6c893543c5a60c6ff5efe8ee13142de16bfece62
bmotadev <bmotadev@gmail.com>
2022-11-03 14:58:21 -0300

    introducao ao while

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -1,13 +1,47 @@
 ﻿using ExemploFundamentos.Models;
 
 int numero = 5;
+int contador = 0;
 
 
-
-for(int contador = 0; contador <= 10; contador++) 
+while (contador <= 10)
 {
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+
+// for(int contador = 0; contador <= 10; contador++) 
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
 
 
 
