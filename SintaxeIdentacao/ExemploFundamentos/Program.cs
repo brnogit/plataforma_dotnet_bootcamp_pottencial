1d8485ec2ee19258b90042aaf36cae66eb8197a4
bmotadev <bmotadev@gmail.com>
2022-11-02 22:57:12 -0300

    incremento e decremento

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -6,10 +6,25 @@ Calculadora calc = new Calculadora();
 // calc.Subtrair(10, 50);
 // calc.Multiplicar(15, 45);
 // calc.Dividir(2, 2);
-calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+
+Console.WriteLine("Incrementando o 10");
+//numero = numero + 1;
+numeroIncremento++;
+
+int numeroDecremento = 20;
+Console.WriteLine("Decrementando o 20");
+//numero = numero + 1;
+numeroDecremento--;
+
+Console.WriteLine(numeroDecremento);
 
 
 
