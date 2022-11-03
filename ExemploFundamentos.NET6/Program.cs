b9e72fa9a5e5c6626d4733175b95fcc5c67e256f
bmotadev <bmotadev@gmail.com>
2022-11-03 19:59:10 -0300

    net6 com metodo main

--- ExemploFundamentos.NET6/Program.cs
+++ ExemploFundamentos.NET6/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ExemploFundamentos.NET6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World com método main no NET6!");
+        }
+    }
+}
