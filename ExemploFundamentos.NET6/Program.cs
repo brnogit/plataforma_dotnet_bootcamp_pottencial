d63d7af4e9fa377da525b0a594393eb02a919dca
bmotadev <bmotadev@gmail.com>
2022-11-03 19:59:43 -0300

    comparando NET5 e NET6

--- ExemploFundamentos.NET6/Program.cs
+++ ExemploFundamentos.NET6/Program.cs
@@ -1,12 +1,2 @@
-﻿using System;
-
-namespace ExemploFundamentos.NET6
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World com método main no NET6!");
-        }
-    }
-}
+﻿
+ Console.WriteLine("Hello World com método main no NET6!");
