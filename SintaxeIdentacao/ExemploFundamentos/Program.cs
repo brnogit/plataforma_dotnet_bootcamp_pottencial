f534615c46db9719ee1885dfd38944fff8c2dd80
bmotadev <bmotadev@gmail.com>
2022-11-03 15:16:38 -0300

    debugando o while

--- SintaxeIdentacao/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/Program.cs
@@ -6,7 +6,7 @@ int contador = 0;
 
 while (contador <= 10)
 {
-    Console.WriteLine($"{contador} Execução: {numero} x {contador} = {numero * contador}");
+    Console.WriteLine($"{contador + 1} Execução: {numero} x {contador} = {numero * contador}");
     contador++;
 
 }
