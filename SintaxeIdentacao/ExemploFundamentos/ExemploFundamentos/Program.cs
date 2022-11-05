928cf31de0ffff4011b311014baf3c8613cfcafb
bmotadev <bmotadev@gmail.com>
2022-11-04 22:24:26 -0300

    trabalhando com listas

--- SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -5,21 +5,31 @@ List<string> listaString = new List<string>();
 listaString.Add("SP");
 listaString.Add("BA");
 listaString.Add("MG");
+listaString.Add("RG");
 
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
-Console.WriteLine("Percorrendo a lista com o FOR");
-for(int contador = 0; contador < listaString.Count; contador++)
-{
-    Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
-}
+listaString.Add("SC");
 
-Console.WriteLine("Percorrendo a lista com o FOREACH");
-int contadorForeach = 0;
-foreach(string item in listaString)
-{
-    Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
-    contadorForeach++;
-}
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+listaString.Remove("MG");
+
+Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+// Console.WriteLine("Percorrendo a lista com o FOR");
+// for(int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo a lista com o FOREACH");
+// int contadorForeach = 0;
+// foreach(string item in listaString)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {item}");
+//     contadorForeach++;
+// }
 
 
 
