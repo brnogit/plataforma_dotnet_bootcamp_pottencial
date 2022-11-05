7479b4f6c14830480c6a26edacb2873f5c6cb5ce
bmotadev <bmotadev@gmail.com>
2022-11-04 22:11:12 -0300

    copiando um array para outro

--- SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
+++ SintaxeIdentacao/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -1,23 +1,64 @@
 ﻿using ExemploFundamentos.Common.Models;
 
-
-int[] arrayInteiros = new int[4];
-
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
-arrayInteiros[3] = 1;
-
-Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+List<string> listaString = new List<string>();
 
 
 
-Console.WriteLine("Percorrendo o Array com o FOR");
 
-for(int contador = 0; contador < arrayInteiros.Length; contador++)
-{
-    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int[4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+// int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+// //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+
+
+// Console.WriteLine("Percorrendo o Array com o FOR");
+
+// for(int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+// }
 
 
 
